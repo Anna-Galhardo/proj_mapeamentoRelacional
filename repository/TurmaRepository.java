848e57a2769fea80cbf1d33c78b3d3159541e986
Anna-Galhardo <annaju.galhardo@gmail.com>
2026-04-28 11:47:52 -0300

    MApeamento relacional

---
+++ repository/TurmaRepository.java
@@ -0,0 +1,11 @@
+package com.mapeamentoRelacional.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mapeamentoRelacional.entity.Turma;
+
+
+
+public interface TurmaRepository extends JpaRepository<Turma, Long> {
+
+}
